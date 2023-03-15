d68a73b476954c85aa4e6e438d72a5bba1401e17
Gimena Di Stefano Nicolina <gimedistefano@gmail.com>
2023-03-15 05:19:05 -0300

    filtros en main, checks dinamicos

---
+++ assets/scripts/details.js
@@ -0,0 +1,41 @@
+// traigo del html
+// const divCards = document.querySelector('.cards'); //contenedor de cards
+
+//  function renderDetail(event){
+//     let cardDetail = '';
+
+  
+//     cardDetail = ` <div class="containerDetailData">
+  //                 <figure class="imageDetail">
+  //                     <img src="${event.image}" alt="cinema">
+  //                 </figure>
+                  
+  //                 <section class="textDetail">
+  //                     <div>
+  //                         <h2>${event.name}</h2>
+  //                         <p>Event date: <time datetime="2023-03-07">${event.date}</time></p>
+  //                         <p>Place: <span class="place">${event.place}</span></p>
+  //                     </div>
+  //                     <div>
+  //                         <p class="description">${event.description}</p>
+  //                     </div>
+  //                 <div>
+  //                     <div class="persons">
+  //                         <span>Capacity: <span class="total">${event.capacity}</span> p.</span>
+  //                         <span>Assistance estimate: <span class="estimados">${event.assistance}</span> p.</span>
+  //                     </div>
+  //                     <p class="containerPrice"><mark class="price">$${event.price}</mark></p>
+  //                     <p class="category" id="indoor">${event.category}</p>
+  //                 </div>
+                      
+  //                 </section>
+  
+  //                 <div class="d-grid gap-2 d-md-flex justify-content-md-end" id="btnDetail" >
+  //                     <button type="button" class="btn btn-outline-secondary" onclick="location.href='./index.html'">Return to Events</button>
+  //                     <button class="btn btn-primary" type="button" onclick="location.href='./contact.html'">Consult</button>
+  //                 </div>
+                  
+  //             </div>
+//     `;
+//     divCards.innerHTML = cardDetail
+// }
--- assets/scripts/past_Events.js
+++ assets/scripts/past_Events.js
@@ -1,45 +1,88 @@
-// CODIGO PARA MOSTRAR DIFERENTES FILTROS
-    // const divCategorias = document.querySelector('.categorias');
+// traigo del html
+const divCards = document.querySelector('.cards'); //contenedor de cards
+const inputSearch = document.getElementById("search"); //traigo el input
+const divCategorias = document.querySelector('.categorias'); //contenedor de checks
 
-    // let checks = '';
-    // for(let categoria of data.events){
-    //     checks+=`
-    //     <div class="form-check">
-    //         <input class="form-check-input" type="checkbox" value="" id="1">
-    //         <label class="form-check-label" for="1">
-    //             ${categoria.category}
-    //         </label>
-    //     </div>` 
-    // }
-    // divCategorias.innerHTML = checks
+//ESCUCHO EVENTOS
 
-// CODIGO PARA MOSTRAR DIFERENTES CARD DE ACUERDO A LA FECHA
+inputSearch.addEventListener('input', filterCombined); //escucho el e del search
+divCategorias.addEventListener('change',filterCombined); //escucho el e del checks
 
-let currentDate = new Date(data.currentDate); //obtengo la fecha de corte
+// FUNCIONES 
+function filterCombined(){ //combino filtros
+    let filterSearch = searchEvent(data.events,inputSearch.value); //filtro de search
+    let filterChecks = filterCategory(filterSearch); //filtro de checks al search
+    renderCards(filterChecks);
+}
 
-function renderCards(data) {
+function renderCards(data) { // CODIGO PARA MOSTRAR DIFERENTES CARD DE ACUERDO A LA FECHA
     let cardsEventos = ''; //creo las card 
-    const divCards = document.querySelector('.cards'); //llamo al elemento del html
-
-            for(let i = 0; i < data.events.length; i++){ //recorro todos los e de data
-                let datos = data.events[i] //traigo los elementos
-                let fecha = new Date(datos.date); //obtengo la fecha del evento
-                if(currentDate > fecha){ //aca si la fecha es mayor cargo las card
-                    cardsEventos +=	`
+    let currentDate = new Date(data.currentDate); //obtengo la fecha de corte
+    data.events.forEach(function(event) {
+        var fecha = new Date(event.date);//obtengo la fecha del evento
+        
+    if (fecha < currentDate) { // Comparo las fechas
+        cardsEventos +=	`
                     <div class="card" style="width: 18rem;">
-                        <img src="${datos.image}"  class="card-img-top" alt="cinema">
+                        <img src="${event.image}"  class="card-img-top" alt="cinema">
                         <div class="card-body">
-                            <h5 class="card-title">${datos.name}</h5>
-                            <p class="card-text">${datos.description}</p>
+                            <h5 class="card-title">${event.name}</h5>
+                            <p class="card-text">${event.description}</p>
                             <div class="btnCard">
-                                <span class="price">$ ${datos.price}</span>
-                                <a href="./details.html" class="btn btn-primary">See more</a>
+                                <span class="price">$ ${event.price}</span>
+                                <a href="./details.html?id=${event._id}" class="btn btn-primary" id= ${event._id}>See more</a>
                             </div>
                         </div>
                     </div>`
-                }
-            }
-            divCards.innerHTML = cardsEventos
-            // console.log(cardsEventos);
+      }else{
+        divCards.innerHTML = `<h2 class="text-white fw-bolder">Try again, no events requested</h2>`
+        return
+      }
+    });
+    divCards.innerHTML = cardsEventos
+}
+
+function renderChecks(data){  //para mostrar los checks
+    let setCategorias = new Set(data.events.map( event => event.category)) //con el map, me traigo todas las cateGorias del DATA y con el SET, hago que no se repitan
+    //console.log(setCategorias)
+    let todosChecks = Array.from(setCategorias) //creo un array con las categorias del DATA filtradas
+    //console.log(todosChecks)
+    let opcionesChecks = [] //array donde iran cada check
+    //console.log(opcionesChecks)
+    todosChecks.forEach(categoria => {
+        opcionesChecks +=`
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="${categoria}" value="${categoria}">
+                <label class="form-check-label" for="${categoria}">
+                    ${categoria}
+                </label>
+            </div>` 
+    });
+    divCategorias.innerHTML = opcionesChecks
+}
+
+function searchEvent(eventos,input){ //para buscar escribiendo
+    let inputEntered = input.toLowerCase(); //al input lo paso a minuscula
+    console.log(inputEntered);
+    let eventsFiltrados = eventos.filter(evento => evento.name.toLowerCase().includes(inputEntered));
+    console.log(eventsFiltrados);
+    return eventsFiltrados
+}
+
+function filterCategory(opcion){ //para buscar por checks
+    let inputsChecks = Array.from(document.querySelectorAll("input[type='checkbox']")) //traigo todos los check del html para usar y lo convierto en array
+    //console.log(inputsChecks);
+    let checksElegidos = inputsChecks //creo una variable donde voy a guardar las opciones
+        .filter(input => input.checked) //que estan seleccionadas
+        .map( input => input.value) //y me traigo el nombre
+    //console.log(checksElegidos);
+    let opcionesFiltradas = opcion.filter(check => checksElegidos.includes(check.category))
+    //console.log(opcionesFiltradas);
+    if(checksElegidos.length > 0){
+        return opcionesFiltradas
     }
-    renderCards(data);
+    return opcion
+}
+
+renderCards(data); //llamo a la function > muestro las cards
+renderChecks(data); //llamo a la function > muestro los checks
