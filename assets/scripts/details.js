7b23ae8ed68683ef50475196440d276e67ee7cc6
Gimena Di Stefano Nicolina <gimedistefano@gmail.com>
2023-03-29 18:14:53 -0300

    correccion devolucion

--- assets/scripts/details.js
+++ assets/scripts/details.js
@@ -59,32 +59,32 @@ divDetail.innerHTML = `
     <figure class="imageDetail">
         <img src="${eventos.image}" alt="cinema">
     </figure>
-    
-    <section class="textDetail">
+    <div class="infoDetail">
+        <section class="textDetail">
+            <div>
+                <h2 class="fw-bold" >${eventos.name}</h2>
+                <p>Event date: <time class="fw-bold" datetime="${eventos.date}">${eventos.date}</time></p>
+                <p>Place: <span class="place fw-bold" >${eventos.place}</span></p>
+            </div>
+            <div>
+                <p class="description">${eventos.description}</p>
+            </div>
         <div>
-            <h2 class="fw-bold" >${eventos.name}</h2>
-            <p>Event date: <time class="fw-bold" datetime="${eventos.date}">${eventos.date}</time></p>
-            <p>Place: <span class="place fw-bold" >${eventos.place}</span></p>
+            <div class="persons">
+                <span>Capacity: <span class="total fw-bold">${eventos.capacity}</span> p.</span>
+                <span>Assistance estimate: <span class="estimados fw-bold">${eventos.assistance}</span> p.</span>
+            </div>
+            <p class="containerPrice"><mark class="price">$${eventos.price}</mark></p>
+            <p class="category" id="indoor">${eventos.category}</p>
         </div>
-        <div>
-            <p class="description">${eventos.description}</p>
-        </div>
-    <div>
-        <div class="persons">
-            <span>Capacity: <span class="total fw-bold">${eventos.capacity}</span> p.</span>
-            <span>Assistance estimate: <span class="estimados fw-bold">${eventos.assistance}</span> p.</span>
-        </div>
-        <p class="containerPrice"><mark class="price">$${eventos.price}</mark></p>
-        <p class="category" id="indoor">${eventos.category}</p>
-    </div>
-        
-    </section>
+            
+        </section>
 
-    <div class="d-grid gap-2 d-md-flex justify-content-md-end" id="btnDetail" >
-        <button type="button" class="btn btn-outline-secondary" onclick="location.href='./index.html'">Return to Events</button>
-        <button class="btn btn-primary" type="button" onclick="location.href='./contact.html'">Consult</button>
-    </div>
-                    
+        <div class="d-grid gap-2 d-md-flex justify-content-md-end" id="btnDetail" >
+            <button type="button" class="btn btn-outline-secondary" onclick="location.href='./index.html'">Return to Events</button>
+            <button class="btn btn-primary" type="button" onclick="location.href='./contact.html'">Consult</button>
+        </div>
+    </div>              
   </div>
 `
 }
